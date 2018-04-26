6686fd41959074d964e15aaf3757f86ee789f496
Aaron Johnson <ajohnson13.aj@gmail.com>
2018-04-25 21:14:59 -0500

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  # def controller_component
+  #   render react_component: 'Hello', props: { name: 'a component rendered from a controller' }
+  # end
+end
