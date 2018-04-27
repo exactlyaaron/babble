85f6419ac7c3dbf2241760836f627893ad930b3e
Aaron Johnson <ajohnson13.aj@gmail.com>
2018-04-26 23:22:51 -0500

    more front end

--- app/javascript/components/home.js
+++ app/javascript/components/home.js
@@ -1,6 +1,106 @@
 import React from 'react';
 
 class Home extends React.Component {
+  renderPopularTags = () => {
+    return (
+      <div className="popular-tags__wrapper">
+        <span className="popular-tags__label">Popular Tags</span>
+        <div className="popular-tags__tags">
+          <span className="popular-tags__tag"><span>Apple</span></span>
+          <span className="popular-tags__tag"><span>Android</span></span>
+          <span className="popular-tags__tag"><span>Things</span></span>
+          <span className="popular-tags__tag"><span>Windows</span></span>
+        </div>
+      </div>
+    )
+  }
+
+  renderTopArticles = (type) => {
+    return (
+      <div className={`top-articles ${type}`}>
+        <span className="top-articles__label">{type.charAt(0).toUpperCase()+type.substr(1)} Articles</span>
+        <div className="top-articles-list">
+          {this.renderMiniArticles(type)}
+        </div>
+      </div>
+    )
+  }
+
+  renderMiniArticles = (type) => {
+    return (
+      <div>
+        <div className="article__wrapper mini">
+          <div className="button__copy"></div>
+          <div className="button__toggle-view expand"></div>
+          <span className="article-title">Opening Blob with a stupidly long title because stuff</span>
+        </div>
+
+        <div className="article__wrapper mini">
+          <div className="button__copy"></div>
+          <div className="button__toggle-view expand"></div>
+          <span className="article-title">Greeting</span>
+        </div>
+
+        <div className="article__wrapper mini">
+          <div className="button__copy"></div>
+          <div className="button__toggle-view expand"></div>
+          <span className="article-title">Closing</span>
+        </div>
+      </div>
+    )
+  }
+
+  renderMainTabs = () => {
+    return (
+      <div className="main-tabs__wrapper">
+        <div className="main-tab active">TAB ONE</div>
+        <div className="main-tab">TAB TWO</div>
+        <div className="main-tab">TAB THREE</div>
+      </div>
+    )
+  }
+
+  renderMainArticles = () => {
+    return (
+      <div className="main-articles__list">
+        <div className="article__wrapper closed">
+          <div className="article__controls">
+            <div className="button__copy"></div>
+            <div className="button__toggle-view expand"></div>
+            <div className="button__toggle-view open"></div>
+            <span className="article-title">i need internet</span>
+          </div>
+          <div className="article__contents__wrapper">
+            <div className="article__content">
+              <div className="button__copy"></div>
+              <div className="article-content__text">
+                I am text for an article about why internet is missing. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tincidunt nibh a mi fringilla, id facilisis tellus lacinia. Curabitur bibendum leo a pharetra convallis. Praesent lobortis mi sit amet ex faucibus facilisis. Integer sed bibendum dolor. Praesent eget lorem scelerisque, porta sem in, rhoncus augue. Nulla faucibus mauris vel turpis tempor efficitur. Quisque molestie nisi aliquet justo semper tincidunt in ac nulla. Sed ullamcorper volutpat purus. In dolor turpis, tincidunt et ornare sit amet, cursus quis turpis. Aliquam ullamcorper varius nisi, in molestie augue. Nulla facilisis a elit vel varius. Quisque lacinia efficitur neque id ultricies. Praesent sed maximus tellus, eget semper enim.
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="article__wrapper opened">
+          <div className="article__controls">
+            <div className="button__copy"></div>
+            <div className="button__toggle-view expand"></div>
+            <div className="button__toggle-view open"></div>
+            <span className="article-title">i need internet</span>
+          </div>
+          <div className="article__contents__wrapper">
+            <div className="article__content">
+              <div className="button__copy"></div>
+              <div className="article-content__text">
+                I am text for an article about why internet is missing. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tincidunt nibh a mi fringilla, id facilisis tellus lacinia. Curabitur bibendum leo a pharetra convallis. Praesent lobortis mi sit amet ex faucibus facilisis. Integer sed bibendum dolor. Praesent eget lorem scelerisque, porta sem in, rhoncus augue. Nulla faucibus mauris vel turpis tempor efficitur. Quisque molestie nisi aliquet justo semper tincidunt in ac nulla. Sed ullamcorper volutpat purus. In dolor turpis, tincidunt et ornare sit amet, cursus quis turpis. Aliquam ullamcorper varius nisi, in molestie augue. Nulla facilisis a elit vel varius. Quisque lacinia efficitur neque id ultricies. Praesent sed maximus tellus, eget semper enim.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+
   render() {
     return (
       <div>
@@ -11,71 +111,14 @@ class Home extends React.Component {
           <div className="search__wrapper">
             <input type="text" placeholder="What do you want" />
           </div>
-          <div className="popular-tags__wrapper">
-            <span className="popular-tags__label">Popular Tags</span>
-            <div className="popular-tags__tags">
-              <span className="popular-tags__tag"><span>Apple</span></span>
-              <span className="popular-tags__tag"><span>Android</span></span>
-              <span className="popular-tags__tag"><span>Things</span></span>
-              <span className="popular-tags__tag"><span>Windows</span></span>
-            </div>
-          </div>
+          {this.renderPopularTags()}
+
           <div className="top-articles__wrapper">
-            <div className="top-articles favorite">
-              <span className="top-articles__label">Favorite Articles</span>
-              <div className="top-articles-list">
-
-                <div className="article__wrapper mini">
-                  <div className="button__copy"></div>
-                  <div className="button__toggle-view expand"></div>
-                  <span className="article-title">Opening Blob with a stupidly long title because stuff</span>
-                </div>
-
-                <div className="article__wrapper mini">
-                  <div className="button__copy"></div>
-                  <div className="button__toggle-view expand"></div>
-                  <span className="article-title">Greeting</span>
-                </div>
-
-                <div className="article__wrapper mini">
-                  <div className="button__copy"></div>
-                  <div className="button__toggle-view expand"></div>
-                  <span className="article-title">Closing</span>
-                </div>
-
-              </div>
-            </div>
-            <div className="top-articles popular">
-              <span className="top-articles__label">Popular Articles</span>
-              <div className="top-articles-list">
-
-                <div className="article__wrapper mini">
-                  <div className="button__copy"></div>
-                  <div className="button__toggle-view expand"></div>
-                  <span className="article-title">android slow</span>
-                </div>
-
-                <div className="article__wrapper mini">
-                  <div className="button__copy"></div>
-                  <div className="button__toggle-view expand"></div>
-                  <span className="article-title">stuff broke</span>
-                </div>
-
-                <div className="article__wrapper mini">
-                  <div className="button__copy"></div>
-                  <div className="button__toggle-view expand"></div>
-                  <span className="article-title">i need internet</span>
-                </div>
-
-              </div>
-            </div>
+            {this.renderTopArticles('favorite')}
+            {this.renderTopArticles('popular')}
           </div>
 
-          <div className="main-tabs__wrapper">
-            <div className="main-tab active">TAB ONE</div>
-            <div className="main-tab">TAB TWO</div>
-            <div className="main-tab">TAB THREE</div>
-          </div>
+          {this.renderMainTabs()}
 
         </div>
 
@@ -85,22 +128,7 @@ class Home extends React.Component {
             <span className="main-articles__control">Collapse All</span>
           </div>
 
-          <div className="main-articles__list">
-            <div className="article__wrapper">
-              <div className="article__controls">
-                <div className="button__copy"></div>
-                <span className="article-title">i need internet</span>
-                <div className="button__toggle-view open"></div>
-                <div className="button__toggle-view expand"></div>
-              </div>
-              <div className="article__contents__wrapper">
-                <div className="article__content">
-                  <div className="button__copy"></div>
-                  <div className="article-content__text">I am text for an article</div>
-                </div>
-              </div>
-            </div>
-          </div>
+          {this.renderMainArticles()}
 
         </div>
         <p>Hello, I am {this.props.name}!</p>
